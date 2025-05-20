6336f126e8823a72ade3e9669a911919eee7493a
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-20 20:27:44 +0530

    repo

--- src/Thirteenmay.java
+++ src/Thirteenmay.java
@@ -14,6 +14,15 @@ public class Thirteenmay {
             System.out.println();
         }
         System.out.println();
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
+
 }
 
