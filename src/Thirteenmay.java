afc6a15f590ca8ce15a3d64391cc7e914038a4d9
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-20 20:54:00 +0530

    pattern questions practice

--- src/Thirteenmay.java
+++ src/Thirteenmay.java
@@ -1,8 +1,8 @@
 public class Thirteenmay {
     public static void main(String[] args) {
 //        print oyramid
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -10,8 +10,8 @@ public class Thirteenmay {
         System.out.println();
 
 //        print inverse pyramid
-        for(int i=5;i>=1;i--){
-            for(int j=1;j<=i;j++){
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -19,8 +19,8 @@ public class Thirteenmay {
         System.out.println();
 
 //print number pattern
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();
@@ -29,8 +29,8 @@ public class Thirteenmay {
 
 //        print character pattern
         char ch = 'A';
-        for(int i=1;i<=10;i++){
-            for(int j=1;j<=i;j++){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(ch);
                 ch++;
             }
@@ -39,11 +39,11 @@ public class Thirteenmay {
         System.out.println();
 
 //        print hollow triangle
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                if(j==1 || j==i || i==1 || i==5){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == 1 || i == 5) {
                     System.out.print("*");
-                }else {
+                } else {
                     System.out.print(" ");
                 }
             }
@@ -54,17 +54,19 @@ public class Thirteenmay {
         //        print hollow rectangle
         int n = 8;
         int m = 8;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(j==1 || j==m || i==1 || i==n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (j == 1 || j == m || i == 1 || i == n) {
                     System.out.print("*");
-                }else {
+                } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
         System.out.println();
+//        INVERTED & ROTATED HALF-PYRAMID pattern
+
     }
 
 }
