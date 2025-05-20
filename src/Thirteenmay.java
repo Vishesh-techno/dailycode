4d20e7693e8e633a1de68b7097f4fcf084ff899f
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-20 21:01:58 +0530

    pattern questions practice

--- src/Thirteenmay.java
+++ src/Thirteenmay.java
@@ -66,6 +66,18 @@ public class Thirteenmay {
         }
         System.out.println();
 //        INVERTED & ROTATED HALF-PYRAMID pattern
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
 
     }
 
