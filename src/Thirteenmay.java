8948289142135baa4950dfa044f1e7c220e6447f
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-21 18:24:40 +0530

    pattern questions practice

--- src/Thirteenmay.java
+++ src/Thirteenmay.java
@@ -154,6 +154,24 @@ public class Thirteenmay {
         }
         System.out.println();
 
+//        HOLLOW RHOMBUS pattern
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= 5; k++) {
+                if (i==1||i==5||k==1||k==5){
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
     }
 
 }
