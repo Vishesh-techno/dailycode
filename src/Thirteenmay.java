8c063037db86dd1dd3a223fb15694ea5b15ae064
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-21 18:50:53 +0530

    pattern questions practice

--- src/Thirteenmay.java
+++ src/Thirteenmay.java
@@ -172,6 +172,29 @@ public class Thirteenmay {
         }
         System.out.println();
 
+//        DIAMOND pattern
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2*i)-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int a = 5; a >= 1; a--) {
+            for (int b = 1; b <= 5-a; b++) {
+                System.out.print(" ");
+            }
+
+            for (int c = 1; c <= (2 * a) - 1; c++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
     }
 
 }
