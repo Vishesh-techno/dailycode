fe171c6bdeebf51df37d886f602e025bae1d79dd
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-22 14:08:55 +0530

    basic practices

---
+++ src/Twentytwo.java
@@ -0,0 +1,14 @@
+public class Twentytwo {
+    public static void main(String[] args) {
+//        Sum of first n natural numbers
+        int n = 100;
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+
+    }
+}
