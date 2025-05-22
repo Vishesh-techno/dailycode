079d1624fd334294a23a3cd681674bea5bc5231c
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-22 14:58:11 +0530

    basic practices

--- src/Twentytwo.java
+++ src/Twentytwo.java
@@ -23,5 +23,37 @@ public class Twentytwo {
             System.out.print(rem);
             n1 /= 10;
         }
+        System.out.println();
+//        reverse a number
+        int rev = 0;
+        int n2 = 12509;
+        while (n2 > 0) {
+            int ld = n2 % 10;
+            rev = rev * 10 + ld;
+            n2 = n2 / 10;
+        }
+        System.out.println("rev is: " + rev);
+
+        for (int i3 = 0; i3 < 5; i3++) {
+            System.out.println("Hello");
+            i3 += 2;
+        }
+        System.out.println();
+//Write a program that reads a set of integers, and then prints the sum of the
+//even and odd integers.
+        int a = 5565421;
+        int evensum = 0;
+        int oddsum = 0;
+        while (a > 0) {
+            int lsd = a % 10;
+            if (lsd % 2 == 0) {
+                evensum += lsd;
+            } else {
+                oddsum += lsd;
+            }
+            a /= 10;
+        }
+        System.out.println("Sum of even digits: " + evensum);
+        System.out.println("Sum of odd digits: " + oddsum);
     }
 }
