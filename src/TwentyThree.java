90412042049f953bf427d225bd1329ae5972413d
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-23 15:34:13 +0530

    basic practices

--- src/TwentyThree.java
+++ src/TwentyThree.java
@@ -11,39 +11,54 @@ public class TwentyThree {
         System.out.print("Element not found ");
         return -1;
     }
+
     public static int largestaray(int[] numbers) {
         int largest = Integer.MIN_VALUE;
         int smallest = Integer.MAX_VALUE;
-        for (int i = 1; i < numbers.length; i++) {
-            if(largest < numbers[i]){
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
                 largest = numbers[i];
-            }else if (smallest > numbers[i]){
+            }
+            if (smallest > numbers[i]) {
                 smallest = numbers[i];
-                System.out.println("the smallest number is " + smallest);
             }
         }
+        System.out.println("the smallest number is " + smallest);
         return largest;
     }
+
     public static int BinarySearch(int[] numbers, int key) {
         int start = 0;
         int end = numbers.length;
         int mid;
-        while (start <= end){
-            mid = (start+end)/2;
-            if (numbers[mid] ==key){
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (numbers[mid] == key) {
                 return mid;
-            }else if (numbers[mid] < key){
-                start = mid+1;
-            }else {
-                end = mid-1;
+            } else if (numbers[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
     }
 
+    public static void ReverseArray(int[] numbers) {
+        int first = 0, last = numbers.length - 1;
+        while (first < last) {
+            int temp = numbers[first];
+            numbers[first] = numbers[last];
+            numbers[last] = temp;
+            first++;
+            last--;
+
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] numbers = {2, 4, 6, 8, 0, 20, 10, 5};
+        int[] numbers = {2, 4, 6, 8, 10, 20, 40, 50};
         int choice;
         do {
             System.out.print("Enter the number to be searched: ");
@@ -51,7 +66,8 @@ public class TwentyThree {
             System.out.println(LinearSearch(numbers, key));
             System.out.print("Do you want to continue? Press 1 for yes or 0 for no: ");
             choice = sc.nextInt();
-        }while (choice == 1) ;
+        } while (choice == 1);
+
         System.out.println("The largest number in the array is " + largestaray(numbers));
         int choice2;
         do {
@@ -60,7 +76,12 @@ public class TwentyThree {
             System.out.println("the key found at index: " + BinarySearch(numbers, key1));
             System.out.print("Do you want to continue? Press 1 for yes or 0 for no: ");
             choice2 = sc.nextInt();
-        }while (choice2 == 1) ;
+        } while (choice2 == 1);
+        ReverseArray(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+ " ");
+        }
         System.out.println("Program ended.");
+
     }
 }
