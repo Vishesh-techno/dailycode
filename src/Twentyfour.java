9748125e132aa2bb067fc6d03c17fa3f771b6409
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-24 16:42:53 +0530

    basic practices

--- src/Twentyfour.java
+++ src/Twentyfour.java
@@ -37,9 +37,50 @@ public class Twentyfour {
         }
         System.out.println("Max Sum by prefix = " + maxSum);
     }
+    public static void kadane(int[] nums) {
+        int crSum = 0;
+        int mxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            crSum += nums[i];
+            if(crSum < 0){
+                crSum = 0;
+            }
+            mxSum = Math.max(mxSum, crSum);
+        }
+        System.out.println("Max Sum using kadanes's algorithm = " + mxSum);
+    }
+
+    public static void kadaneforall(int[] nums) {
+        int crSum = 0;
+        int mxSum = Integer.MIN_VALUE;
+        boolean allNegative = true;
+        int maxNegative = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                allNegative = false;
+            }
+            if (nums[i] > maxNegative) {
+                maxNegative = nums[i];
+            }
+            crSum += nums[i];
+            if (crSum < 0) {
+                crSum = 0;
+            }
+            mxSum = Math.max(mxSum, crSum);
+        }
+
+        if (allNegative) {
+            System.out.println("Max Sum (All Negative) = " + maxNegative);
+        } else {
+            System.out.println("Max Sum using Kadane's algorithm = " + mxSum);
+        }
+    }
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         maxSubArray(nums);
         prefixMaxSubArray(nums);
+        kadane(nums);
+        kadaneforall(nums);
     }
 }
