5c6a06ae1236a4186d52d3122890096fa5508e02
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-30 14:34:23 +0530

    Revision of array

--- src/ThirtyMay.java
+++ src/ThirtyMay.java
@@ -1,5 +1,16 @@
-public class ThirtyMay {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class ThirtyMay {
+    public static int minandmax(int[] nums){
+        Arrays.sort(nums);
+        int secondMin = nums[2];
+        int secondMax = nums[nums.length-2];
+        System.out.println(secondMin);
+        return secondMax;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,4,5,5,6,78,6,5,6,5,-5,-6,415,0,45};
+        System.out.println(minandmax(nums));
     }
 }
