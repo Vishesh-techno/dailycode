8af5cda39b2095e2650ec3616fc86335e1eef40b
Vishesh-techno <sonivishesh099@gmail.com>
2025-05-30 15:24:11 +0530

    Revision of array

--- src/ThirtyMay.java
+++ src/ThirtyMay.java
@@ -18,11 +18,24 @@ public class ThirtyMay {
         }
         return maxProfit;
     }
+    public static int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int i = 0; i <= prices.length-1; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else if (prices[i] - min > max){
+                max = prices[i] - min;
+            }
+        }
+        return max;
+    }
 
 
     public static void main(String[] args) {
         int[] nums = {2,4,5,5,6,78,6,5,6,5,-5,-6,415,0,45};
         System.out.println(minandmax(nums));
         System.out.println(buyandSellStock(nums));
+        System.out.println(maxProfit(nums));
     }
 }
