c85470c902d005c48edc082121b4440c0a0c225b
Vishesh-techno <sonivishesh099@gmail.com>
2025-06-01 15:02:13 +0530

    Revision of array

--- src/MultiDimesionArray.java
+++ src/MultiDimesionArray.java
@@ -18,8 +18,11 @@ public class MultiDimesionArray {
             }
             System.out.println();
         }
-//        for (int i = 0; i < matrix.length; i++) {
-//            System.out.println(Arrays.toString(matrix[i]));
-//        }
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        for(int[] mat : matrix){
+            System.out.println(Arrays.toString(mat));
+        }
     }
 }
